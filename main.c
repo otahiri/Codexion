5bc202b0ee2f7560098d897df4b59d1b80b75fa4
otahiri <chucy0066@gmail.com>
2026-02-21 17:46:06 +0000

    added initial stuff for codexion

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int int main(int argc, char *argv[])
+{
+	int number_of_coders;
+	int time_to_burnout;
+	int time_to_compile;
+	int time_to_debug;
+	int time_to_refactor;
+	int number_of_compiles_required;
+	int dongle_cooldown;
+	int scheduler;
+	if (argc != 9)
+	{
+		printf("invalid arguments");
+		return (1);
+	}
+}
